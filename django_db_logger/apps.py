1109a9d6eeed52803092a35c17c4858f6557803c
maxwell2011 <42814450+maxwell2011@users.noreply.github.com>
2026-06-19 16:53:39 -0700

    Update apps.py

--- django_db_logger/apps.py
+++ django_db_logger/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class DbLoggerAppConfig(AppConfig):
     name = 'django_db_logger'
-    verbose_name = 'Db logging'
+    verbose_name = "Database Logging"
+    verbose_name_plural = "Database Logging"
     # Explicitly set default auto field type to avoid migrations in Django 3.2+
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = "django.db.models.AutoField"
