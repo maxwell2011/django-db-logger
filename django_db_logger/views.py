5cea31af5e0b85b10e3b14ce8348d5100d167a72
maxwell2011 <42814450+maxwell2011@users.noreply.github.com>
2026-06-19 16:58:12 -0700

    Update views.py

--- django_db_logger/views.py
+++ django_db_logger/views.py
@@ -1,13 +1,83 @@
 import logging
-
-from django.http import HttpResponse
+import json
+from django.forms import ModelForm
+from django.http import HttpRequest, HttpResponse, FileResponse
+from django.template.response import TemplateResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
+from django_htmx.middleware import HtmxDetails
+from import_export.forms import ExportForm
+from .utils import for_htmx
+from .models import StatusLog, TracebackLog
+from .resources import StatusLogResource, TracebackLogResource
+# Create your views here.
 
 logger = logging.getLogger('db')
 
-def __gen_500_errors(request):
-    try:
-        1/0
-    except Exception as e:
-        logger.exception(e)
+class FlushDebugLogsForm(ModelForm):
+    class Meta:
+        model = StatusLog
 
-    return HttpResponse('Hello 500!')
+@for_htmx(use_block_from_params=True)
+def main(request) -> TemplateResponse:
+    return TemplateResponse(
+        request,
+        "server_logs_modal_main.html",
+        {
+            "logs": StatusLog.objects.all().order_by("-asctime"),
+        },
+    )
+
+
+@require_GET
+def logs(request: HtmxHttpRequest) -> HttpResponse:
+    _logs_errors = StatusLog.objects.get_errors()
+    _logs = StatusLog.objects.all()
+    return render(
+        request, 
+        "server_logs.html",
+        {
+            "server_logs": _logs,
+            "server_errors": _logs_errors
+        }
+    )
+
+@require_POST
+def logs_download_start(request: HtmxHttpRequest) -> HttpResponse:
+    return render(
+        request, 
+        "server_logs_download.html"
+    )
+
+@require_GET
+def logs_download_status(request: HtmxHttpRequest) -> HttpResponse:
+    return render(
+        request, 
+        "server_logs_download.html"
+    )
+
+@require_GET
+def logs_download_file(request: HtmxHttpRequest) -> FileResponse:
+    #response = FileResponse(
+    #    open(file_path, "rb"),
+    #    content_type="application/octet-stream",
+    #    filename="server-logs.txt"
+    #)
+    return {} #response
+
+@require_http_methods(["DELETE"])
+def logs_download_reset(request: HtmxHttpRequest) -> HttpResponse:
+    return render(
+        request, "server_logs.html"
+    )
+
+
+@require_GET
+def logs_download_file_error_logs(request: HtmxHttpRequest) -> FileResponse:
+    #response = FileResponse(
+    #    open(file_path, "rb"),
+    #    content_type="application/octet-stream",
+    #    filename="server-logs.txt"
+    #)
+    return {} #response
+    #/logs/download/error_logs
