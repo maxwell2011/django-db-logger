999997127d8204b208b8caffe30c4db851f30b40
maxwell2011 <42814450+maxwell2011@users.noreply.github.com>
2026-06-19 17:03:55 -0700

    Update 0001_initial.py

--- django_db_logger/migrations/0001_initial.py
+++ django_db_logger/migrations/0001_initial.py
@@ -6,6 +6,7 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
+    initial = True
     dependencies = [
     ]
 
@@ -13,16 +14,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='StatusLog',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('logger_name', models.CharField(max_length=100)),
-                ('level', models.PositiveSmallIntegerField(choices=[(0, 'NotSet'), (20, 'Info'), (30, 'Warning'), (10, 'Debug'), (40, 'Error'), (50, 'Fatal')], default=40, db_index=True)),
-                ('msg', models.TextField()),
-                ('trace', models.TextField(null=True, blank=True)),
-                ('create_datetime', models.DateTimeField(auto_now_add=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('asctime', models.DateTimeField(help_text='Timestamp of the log entry', verbose_name='Timestamp')),
+                ('module', models.CharField(help_text='Name portion of the filename', max_length=100, verbose_name='Model')),
+                ('lineno', models.PositiveIntegerField(default=0, help_text='Source line number where the logging call was issued (if available, default: 0)', verbose_name='Line Number')),
+                ('pid', models.PositiveIntegerField(default=0, help_text='Process id (if available, default: 0)', verbose_name='Process ID')),
+                ('tid', models.PositiveIntegerField(default=0, help_text='Thread id (if available, default: 0)', verbose_name='Thread ID')),
+                ('logger', models.CharField(help_text='Name of the logger', max_length=100, verbose_name='Logger')),
+                ('level', models.PositiveSmallIntegerField(choices=[(0, 'NotSet'), (20, 'Info'), (30, 'Warning'), (10, 'Debug'), (40, 'Error'), (50, 'Fatal')], db_index=True, default=40, help_text='Log level of the entry', verbose_name='Level')),
+                ('msg', models.TextField(help_text='Raw message', max_length=4096, verbose_name='Message')),
+                ('trace', models.TextField(blank=True, help_text='Raw traceback exception info (if available)', max_length=4096, null=True, verbose_name='Traceback')),
             ],
             options={
-                'ordering': ('-create_datetime',),
+                'verbose_name': 'Logging',
+                'verbose_name_plural': 'Logging',
+                'ordering': ('-asctime',),
             },
-            bases=(models.Model,),
         ),
     ]
